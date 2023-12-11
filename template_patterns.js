c3ae1743c5b5bf7fdc7a78ef511ac7339debc15e
Asteris Giannoudis <asteris.gian@gmail.com>
2023-12-11 16:05:33 +0200

    Added more templateDoc keys.

--- template_patterns.js
+++ template_patterns.js
@@ -10,30 +10,63 @@ const templateRegexes = {
   exercisesDirDoc:    "SGEN:EXERCISES_DIR",
 
   assetsAsCodeRegex:  /\{\{\s?SGEN:\s?ASSETS_AS_CODE\s?\}\}/gi,
+  assetsAsCodeRegexDoc: "SGEN:ASSETS_AS_CODE",
+
   // TODO: weekRegex will be replaced by weekFullRegex and weekNumRegex
   weekRegex:          /\{\{\s?SGEN:\s?WEEK\s?\}\}/gi,
+  weekRegexDoc:       "SGEN:WEEK",
+
   weekNumRegex:       /\{\{\s?SGEN:\s?WEEK_NUM\s?\}\}/gi,
+  weekNumRegexDoc:    "SGEN:WEEK_NUM",
+
   weekFullRegex:      /\{\{\s?SGEN:\s?WEEK_FULL\s?\}\}/gi,
+  weekFullRegexDoc:   "SGEN:WEEK_FULL",
+
   titleRegex:         /\{\{\s?SGEN:\s?TITLE\s?\}\}/gi,
+  titleRegexDoc:      "SGEN:TITLE",
+
   // TODO: dayRegex will be replaced by dayFullRegex and dayNumRegex
   dayRegex:           /\{\{\s?SGEN:\s?DAY\s?\}\}/gi,
+  dayRegexDoc:        "SGEN:DAY",
+
   dayFullRegex:       /\{\{\s?SGEN:\s?DAY_FULL\s?\}\}/gi,
+  dayFullRegexDoc:   "SGEN:DAY_FULL",
+
   dayNumRegex:        /\{\{\s?SGEN:\s?DAY_NUM\s?\}\}/gi,
+  dayNumRegexDoc:    "SGEN:DAY_NUM",
+
+
   scheduleRegex:      /\{\{\s?SGEN:\s?DAILY_SCHEDULE\s?\}\}/gi,
+  scheduleRegexDoc:   "SGEN:DAILY_SCHEDULE",
+
   studyPlanRegex:     /\{\{\s?SGEN:\s?STUDY_PLAN\s?\}\}/gi,
+  studyPlanRegexDoc:  "SGEN:STUDY_PLAN",
+
   summaryRegex:       /\{\{\s?SGEN:\s?SUMMARY\s?\}\}/gi,
+  summaryRegexDoc:    "SGEN:SUMMARY",
+
   exercisesRegex:     /\{\{\s?SGEN:\s?EXERCISES\s?\}\}/gi,
+  exercisesRegexDoc:  "SGEN:EXERCISES",
+
   extrasRegex:        /\{\{\s?SGEN:\s?EXTRAS\s?\}\}/gi,
+  extrasRegexDoc:     "SGEN:EXTRAS",
+
   attributionsRegex:  /\{\{\s?SGEN:\s?ATTRIBUTIONS\s?\}\}/gi,
+  attributionsRegexDoc: "SGEN:ATTRIBUTIONS",
+
 
   includesRegex:      /\{\{\s?SGEN:\s?INCLUDES:(.*)\s?\}\}/gi,
   includesRegexDoc:   "SGEN:INCLUDES:include_name",
 
   moduleRegex:        /( *?)\{\{\s?SGEN:\s?MODULE:(.*)\s?\}\}/gi,
-  moduleReadRegex:    /( *?)\{\{\s?SGEN:\s?MODULE_READ:(.*)\s?\}\}/gi,
   moduleRegexDoc:     "SGEN:MODULE:some/path_inside_modules/index.md",
 
+  moduleReadRegex:    /( *?)\{\{\s?SGEN:\s?MODULE_READ:(.*)\s?\}\}/gi,
+  moduleReadRegexDoc: '- [Read: **frontMatter.title**](../modules/path/to/index.md){:target="_blank"}',
+
   dateUpdatedRegex:   /\{\{\s?SGEN:\s?DATE_UPDATED\s?\}\}/gi,
+  dateUpdatedRegexDoc: "DD/MM/YYYY",
+
   weeklyContentRegex: /\{\{\s?SGEN:\s?WEEKLY_CONTENT\s?\}\}/gi,
   weeklyContentRegexDoc: "SGEN:WEEKLY_CONTENT",
 
