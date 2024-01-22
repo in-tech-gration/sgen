78a07bb486d3feb3d5d65d40e00d1e66dc4785a5
Asteris Giannoudis <asteris.gian@gmail.com>
2024-01-22 13:24:09 +0200

    Created tests for getFrontMatterStringFromObject

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,24 +1,55 @@
 const test = require("node:test");
 const { equal, deepStrictEqual } = require("node:assert");
-const { getYouTubeListIdParts } = require("../utils"); 
+const {
+  getYouTubeListIdParts,
+  getFrontMatterStringFromObject,
+} = require("../utils");
 
-test('Testing getYouTubeListIdParts()', () => {
+// =================================================================
 
-  const input1 = "https://www.youtube.com/watch?v=JZXQ455OT3A&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw&index=1&pp=iAQB";
+test("Testing getYouTubeListIdParts()", () => {
+  const input1 =
+    "https://www.youtube.com/watch?v=JZXQ455OT3A&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw&index=1&pp=iAQB";
   const output1 = [
-    'https://www.youtube.com/watch?v=JZXQ455OT3A',
-    '&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw',
-    '&index=1&pp=iAQB'
+    "https://www.youtube.com/watch?v=JZXQ455OT3A",
+    "&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw",
+    "&index=1&pp=iAQB",
   ];
-  deepStrictEqual(output1, getYouTubeListIdParts(input1));
-  
-  const input2 = "https://www.youtube.com/playlist?list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw";
-  const output2 =  [
-    'https://www.youtube.com/playlist',
-    '?list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw'
-  ];
-  deepStrictEqual(output2, getYouTubeListIdParts(input2));
+  deepStrictEqual(getYouTubeListIdParts(input1), output1);
 
+  const input2 =
+    "https://www.youtube.com/playlist?list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw";
+  const output2 = [
+    "https://www.youtube.com/playlist",
+    "?list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw",
+  ];
+  deepStrictEqual(getYouTubeListIdParts(input2), output2);
 });
 
 // TODO: Test createFrontMatterMarkdownFromObject()
+// =================================================================
+
+test("Testing 'getFrontMatterStringFromObject' with empty object", () => {
+  const fm1 = {};
+  const output1 = "";
+
+  deepStrictEqual(getFrontMatterStringFromObject(fm1), output1);
+});
+
+test("Testing 'getFrontMatterStringFromObject' with simple object of 2 entries", () => {
+  const fm2 = { id: 1, title: "Test Simple Object" };
+  const output2 = "---\nid: 1\ntitle: Test Simple Object\n---\n";
+
+  deepStrictEqual(getFrontMatterStringFromObject(fm2), output2);
+});
+
+test("Testing 'getFrontMatterStringFromObject' with simple object of 2 entries and liveCodeEnabled", () => {
+  const fm3 = { id: 1, title: "Test Simple Object" };
+  const output3 =
+    "---\nid: 1\ntitle: Test Simple Object\nload_script_js_via_src:\n  - flems/flems.html\n  - flems/flems_init.js\n---\n";
+
+  // Test with simple object and live code enabled
+  deepStrictEqual(getFrontMatterStringFromObject(fm3, true), output3);
+});
+
+// =================================================================
